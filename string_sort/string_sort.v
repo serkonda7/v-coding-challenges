b266c4efedcaa9b459397f27e7954dcc6f006fc6
Lukas Neubert <lukas.neubert3@gmail.com>
2020-06-29 17:02:53 +0200

    string_sort: small formatting

--- string_sort/string_sort.v
+++ string_sort/string_sort.v
@@ -1,24 +1,27 @@
 /*
 Given a string as input, output its characters alphabetically sorted from A to Z.
+
   - Number should go first
   - Special characters, such as symbols, go before numbers and letters
   - Whitespaces should be ignored.
 */
-
 module string_sort
 
 pub fn sort(s string) string {
 	mut chars := s.bytes()
-	quick_sort(mut chars, 0, chars.len-1)
+	quick_sort(mut chars, 0, chars.len - 1)
+
 	mut sorted := ''
 	for c in chars {
 		sorted += c.str()
 	}
+
 	return sorted
 }
 
-fn quick_sort(mut chars []byte, l int, r int) {
+fn quick_sort(mut chars []byte, l, r int) {
 	if l >= r { return }
+
 	mut piv := l
 	for i in l+1..r+1 {
 		if chars[i] < chars[l] {
@@ -31,7 +34,7 @@ fn quick_sort(mut chars []byte, l int, r int) {
 	quick_sort(mut chars, piv+1, r)
 }
 
-fn swap(mut chars []byte, a int, b int) {
+fn swap(mut chars []byte, a, b int) {
 	tmp := chars[a]
 	chars[a] = chars[b]
 	chars[b] = tmp
