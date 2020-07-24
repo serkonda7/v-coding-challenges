6bed4d7788efafaedcccb080b8283643dc291efb
Lukas Neubert <lukas.neubert3@gmail.com>
2020-07-24 15:09:42 +0200

    add collatz_conjecture

---
+++ collatz_conjectue/collatz_conjecture.v
@@ -0,0 +1,43 @@
+/* Collatz Conjecture
+
+The Collatz conjecture (also known as the Ulam conjecture or the Syracuse problem) is an unsolved mathematical problem, which is very easy to formulate:
+
+1. Take any natural number
+2. If it's even, half it, otherwise triple it and add one
+3. Repeat step 2. until you reach 4, 2, 1 sequence
+4. You will ALWAYS reach 1, eventually.
+
+That last sequence: 4, 2, 1 is an infinitely repeating loop. The formulated conjecture is that for any x the sequence will always reach 4, 2, 1 ultimately.
+
+While the problem cannot be proved, the assignment is to write a code that will produce and print out the whole sequence for an input number.
+
+
+Both recursive and iterative approaches count. Bonus points for printing out how many iterations it took for x to converge to 1.
+*/
+module collatz_conjecture
+
+pub fn get_sequence_iterative(num int) ([]string, int) {
+	mut got_one := false
+	mut n := num
+	mut sequence := []string{}
+	mut steps := 0
+	for {
+		if n % 2 == 0 {
+			res := n / 2
+			sequence << '$n / 2 = $res'
+			n = res
+		} else {
+			res := n * 3 + 1
+			sequence << '$n * 3 + 1 = $res'
+			n = res
+		}
+		steps++
+		if got_one {
+			break
+		}
+		if n == 1 {
+			got_one = true
+		}
+	}
+	return sequence, steps
+}
---
+++ collatz_conjectue/collatz_conjecture_test.v
@@ -0,0 +1,17 @@
+module collatz_conjecture
+
+fn test_get_sequence_iterative() {
+	exp_sequence := [
+		'17 * 3 + 1 = 52', '52 / 2 = 26',
+		'26 / 2 = 13', '13 * 3 + 1 = 40',
+		'40 / 2 = 20', '20 / 2 = 10',
+		'10 / 2 = 5', '5 * 3 + 1 = 16',
+		'16 / 2 = 8', '8 / 2 = 4',
+		'4 / 2 = 2', '2 / 2 = 1',
+		'1 * 3 + 1 = 4',
+	]
+	exp_steps := 13
+	sequence, steps := get_sequence_iterative(17)
+	assert sequence == exp_sequence
+	assert steps == exp_steps
+}
