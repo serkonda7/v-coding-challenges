9c3c36feb04a0227e1b568a78a1c6bd1b5a958f5
Lukas Neubert <lukas.neubert3@gmail.com>
2020-08-30 22:07:18 +0200

    summations_calculator

---
+++ summations_calculator/summations_calculator.v
@@ -0,0 +1,54 @@
+/*
+Summations Calculator
+
+Create a program that takes 3 inputs, a lower bound, an upper bound and the expression. Calculate the sum of that range based on the given expression and output the result.
+*/
+module summations_calculator
+
+const (
+	expr_funcs = {
+		'+': add
+		'-': sub
+		'*': mult
+		'x': mult
+		'/': div
+		'%': modulo
+	}
+)
+
+pub fn get_summation(min, max int, expression string) ?int {
+	expr_type := expression[0].str()
+	if expr_type !in expr_funcs {
+		return error('Unknown expression type: $expr_type')
+	}
+	expr_num := expression[1..].int()
+	func := expr_funcs[expr_type]
+	mut sum := 0
+	// In the task the range is inclusive!
+	for i in min .. max + 1 {
+		step := func(i, expr_num)
+		sum += step
+	}
+	return sum
+}
+
+// Mathematic helper functions
+fn add(a, b int) int {
+	return a + b
+}
+
+fn sub(a,b int) int {
+	return a - b
+}
+
+fn mult(a, b int) int {
+	return a * b
+}
+
+fn div(a, b int) int {
+	return a / b
+}
+
+fn modulo(a, b int) int {
+	return a % b
+}
---
+++ summations_calculator/summations_calculator_test.v
@@ -0,0 +1,32 @@
+module summations_calculator
+
+struct Input {
+	min int
+	max int
+	expression string
+}
+
+fn test_get_summation() {
+	inputs := [
+		Input{1, 3, '+3'},
+		Input{10, 12, '-10'},
+		Input{2, 4, '*2'},
+		Input{3, 5, 'x3'},
+		Input{4, 8, '/2'},
+		Input{1, 5, '%2'},
+	]
+	expected := [15, 3, 18, 36, 14, 3]
+	for i, inp in inputs {
+		res := get_summation(inp.min, inp.max, inp.expression) or {
+			panic(err)
+		}
+		assert res == expected[i]
+	}
+}
+
+fn test_errors() {
+	inp := Input{1, 3, 'y3'}
+	get_summation(inp.min, inp.max, inp.expression) or {
+		assert err == 'Unknown expression type: y'
+	}
+}
