4a02999c6d920a3eff6e13ccae782c5167a4ca49
Lukas Neubert <lukas.neubert3@gmail.com>
2020-09-05 11:22:30 +0200

    summations_calculator: handle spaces

--- summations_calculator/summations_calculator_test.v
+++ summations_calculator/summations_calculator_test.v
@@ -9,13 +9,14 @@ struct Input {
 fn test_get_summation() {
 	inputs := [
 		Input{1, 3, '+3'},
+		Input{2, 4, '+ 2'},
 		Input{10, 12, '-10'},
 		Input{2, 4, '*2'},
 		Input{3, 5, 'x3'},
 		Input{4, 8, '/2'},
 		Input{1, 5, '%2'},
 	]
-	expected := [15, 3, 18, 36, 14, 3]
+	expected := [15, 15, 3, 18, 36, 14, 3]
 	for i, inp in inputs {
 		res := get_summation(inp.min, inp.max, inp.expression) or {
 			panic(err)
@@ -25,8 +26,8 @@ fn test_get_summation() {
 }
 
 fn test_errors() {
-	inp := Input{1, 3, 'y3'}
+	inp := Input{1, 3, 'y 3'}
 	get_summation(inp.min, inp.max, inp.expression) or {
-		assert err == 'Unknown expression type: y'
+		assert err == 'Unknown type for expression: `y 3`'
 	}
 }
