1dc3713809566b67c1a2a1b5ddc0bfc4b28d0fcc
Lukas Neubert <serkonda@protonmail.com>
2021-10-02 12:03:52 +0200

    return wrong fields

--- sudoku_validator/sudoku_validator.v
+++ sudoku_validator/sudoku_validator.v
@@ -1,26 +1,50 @@
 module sudoku_validator
 
-const target_sum = 45
-
-pub fn is_valid(sudokus [][]int) ?bool {
-	if sudokus.len != 9 {
-		return error('Expected 9 rows, got $sudokus.len')
+pub fn is_valid(sudoku_rows [][]int) ?(bool, map[int][]int) {
+	if sudoku_rows.len != 9 {
+		return error('Expected 9 rows, got $sudoku_rows.len')
 	}
-	for i, row in sudokus {
+	for i, row in sudoku_rows {
 		if row.len != 9 {
 			return error('Expected 9 columns, but row ${i + 1} only has $row.len')
 		}
 	}
-	mut row_sums := []int{len: 9}
-	mut col_sums := []int{len: 9}
-	mut square_sums := []int{len: 9}
-	for i, row in sudokus {
+
+	mut cols := [][]int{len: 9}
+	for row in sudoku_rows {
 		for j, val in row {
-			row_sums[i] += val
-			col_sums[j] += val
-			sq_idx := (i / 3) + ((j / 3) * 3)
-			square_sums[sq_idx] += val
+			if val < 1 || val > 9 {
+				return false, map[int][]int{}
+			}
+			cols[j] << val
 		}
 	}
-	return row_sums.all(it == 45) && col_sums.all(it == 45) && square_sums.all(it == 45)
+
+	mut wrong_fields := map[int][]int{}
+	for r, row in sudoku_rows {
+		for n in 1 .. 10 {
+			idx1 := row.index(n)
+			idx2 := 8 - row.reverse().index(n)
+			if idx1 >= 0 && idx1 != idx2 {
+				wrong_fields[r] << idx1
+				wrong_fields[r] << idx2
+			}
+		}
+	}
+	for c, col in cols {
+		for n in 1 .. 10 {
+			idx1 := col.index(n)
+			idx2 := 8 - col.reverse().index(n)
+			if idx1 >= 0 && idx1 != idx2 {
+				if c !in wrong_fields[idx1] {
+					wrong_fields[idx1] << c
+				}
+				if c !in wrong_fields[idx2] {
+					wrong_fields[idx2] << c
+				}
+			}
+		}
+	}
+
+	return wrong_fields.keys().len == 0, wrong_fields
 }
--- sudoku_validator/sudoku_validator_test.v
+++ sudoku_validator/sudoku_validator_test.v
@@ -14,33 +14,42 @@ fn test_valid_sudokus() {
 		[5, 3, 9, 6, 8, 2, 1, 4, 7],
 	]
 	for s in sudokus {
-		res := is_valid(s) or {
+		res, _ := is_valid(s) or {
 			eprintln(err.msg)
-			false
+			false, map[int][]int{}
 		}
 		assert res
 	}
 }
 
+struct InvalidInput {
+	sudoku  [][]int
+	indices map[int][]int
+}
+
 fn test_invalid_sudokus() {
-	mut sudokus := [][][]int{}
-	sudokus << [
+	mut inputs := []InvalidInput{}
+	inputs << InvalidInput{[
 		[3, 5, 2, 9, 1, 8, 6, 7, 4],
 		[8, 9, 7, 2, 4, 6, 5, 1, 3],
 		[6, 4, 1, 7, 5, 3, 2, 8, 9],
 		[7, 8, 3, 5, 6, 9, 4, 2, 1],
 		[9, 2, 6, 1, 3, 4, 7, 5, 8],
 		[4, 1, 5, 8, 2, 7, 9, 3, 6],
-		[8, 6, 4, 3, 7, 5, 8, 9, 2],
+		[9, 6, 4, 3, 7, 5, 8, 9, 2],
 		[2, 7, 8, 4, 9, 1, 3, 6, 5],
 		[5, 3, 9, 6, 8, 2, 1, 4, 7],
-	]
-	for s in sudokus {
-		res := !is_valid(s) or {
+	], {
+		4: [0]
+		6: [0, 7]
+	}}
+	for inp in inputs {
+		res, indices := is_valid(inp.sudoku) or {
 			eprintln(err.msg)
-			false
+			false, map[int][]int{}
 		}
-		assert res
+		assert !res
+		assert indices == inp.indices
 	}
 }
 
@@ -50,7 +59,7 @@ struct ErrInput {
 }
 
 fn test_sudoku_errors() {
-	inputs := [
+	mut inputs := [
 		ErrInput{[[0], [1], [2], [3], [4], [5], [6]], 'Expected 9 rows, got 7'},
 		ErrInput{[[0, 1, 2, 3, 4], [1], [2], [3], [4], [5], [6],
 			[7], [8]], 'Expected 9 columns, but row 1 only has 5'},
