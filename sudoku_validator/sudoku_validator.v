d4b8da1a61dfacff35e1d7b248e30204424e2a6d
Lukas Neubert <serkonda@protonmail.com>
2021-10-02 12:09:32 +0200

    sudoku validator: return wrong fields (#11)

--- sudoku_validator/sudoku_validator.v
+++ sudoku_validator/sudoku_validator.v
@@ -1,26 +1,61 @@
+/*
+Sudoku Validator
+
+A solved sudoku puzzle is a matrix of typically 9*9 in which every row, every column and every block contains every number from 1 to 9 exactly once.
+
+A block is a 3*3 area in the sudoku that you get by splitting it up into 9 even squares (similar to a tic-tac-toe board).
+
+Create an algorithm that checks any solved sudoku's validity.
+
+Bonus: Change the code to mark all mistakes in the sudoku puzzle.
+*/
 module sudoku_validator
 
-const target_sum = 45
-
-pub fn is_sudoku_valid(sudoku_rows [][]int) bool {
+pub fn is_valid(sudoku_rows [][]int) ?(bool, map[int][]int) {
 	if sudoku_rows.len != 9 {
-		return false
+		return error('Expected 9 rows, got $sudoku_rows.len')
 	}
-	for row in sudoku_rows {
-		if row.len != 9 {
-			return false
-		}
-	}
-	mut row_sums := []int{len: 9}
-	mut col_sums := []int{len: 9}
-	mut square_sums := []int{len: 9}
 	for i, row in sudoku_rows {
-		for j, val in row {
-			row_sums[i] += val
-			col_sums[j] += val
-			sq_idx := (i / 3) + ((j / 3) * 3)
-			square_sums[sq_idx] += val
+		if row.len != 9 {
+			return error('Expected 9 columns, but row ${i + 1} only has $row.len')
 		}
 	}
-	return row_sums.all(it == 45) && col_sums.all(it == 45) && square_sums.all(it == 45)
+
+	mut cols := [][]int{len: 9}
+	for row in sudoku_rows {
+		for j, val in row {
+			if val < 1 || val > 9 {
+				return false, map[int][]int{}
+			}
+			cols[j] << val
+		}
+	}
+
+	mut wrong_fields := map[int][]int{}
+	for r, row in sudoku_rows {
+		for n in 1 .. 10 {
+			idx1 := row.index(n)
+			idx2 := 8 - row.reverse().index(n)
+			if idx1 >= 0 && idx1 != idx2 {
+				wrong_fields[r] << idx1
+				wrong_fields[r] << idx2
+			}
+		}
+	}
+	for c, col in cols {
+		for n in 1 .. 10 {
+			idx1 := col.index(n)
+			idx2 := 8 - col.reverse().index(n)
+			if idx1 >= 0 && idx1 != idx2 {
+				if c !in wrong_fields[idx1] {
+					wrong_fields[idx1] << c
+				}
+				if c !in wrong_fields[idx2] {
+					wrong_fields[idx2] << c
+				}
+			}
+		}
+	}
+
+	return wrong_fields.keys().len == 0, wrong_fields
 }
--- sudoku_validator/sudoku_validator_test.v
+++ sudoku_validator/sudoku_validator_test.v
@@ -14,27 +14,64 @@ fn test_valid_sudokus() {
 		[5, 3, 9, 6, 8, 2, 1, 4, 7],
 	]
 	for s in sudokus {
-		assert is_sudoku_valid(s)
+		res, _ := is_valid(s) or {
+			eprintln(err.msg)
+			false, map[int][]int{}
+		}
+		assert res
 	}
 }
 
+struct InvalidInput {
+	sudoku  [][]int
+	indices map[int][]int
+}
+
 fn test_invalid_sudokus() {
-	mut sudokus := [][][]int{}
-	sudokus << [[0], [1], [2], [3], [4], [5], [6], [7]] // too few rows
-	sudokus << [[0], [1], [2], [3], [4], [5], [6], [7], [8]] // too few columns
-	// double eight, first col
-	sudokus << [
+	mut inputs := []InvalidInput{}
+	inputs << InvalidInput{[
 		[3, 5, 2, 9, 1, 8, 6, 7, 4],
 		[8, 9, 7, 2, 4, 6, 5, 1, 3],
 		[6, 4, 1, 7, 5, 3, 2, 8, 9],
 		[7, 8, 3, 5, 6, 9, 4, 2, 1],
 		[9, 2, 6, 1, 3, 4, 7, 5, 8],
 		[4, 1, 5, 8, 2, 7, 9, 3, 6],
-		[8, 6, 4, 3, 7, 5, 8, 9, 2],
+		[9, 6, 4, 3, 7, 5, 8, 9, 2],
 		[2, 7, 8, 4, 9, 1, 3, 6, 5],
 		[5, 3, 9, 6, 8, 2, 1, 4, 7],
-	]
-	for s in sudokus {
-		assert !is_sudoku_valid(s)
+	], {
+		4: [0]
+		6: [0, 7]
+	}}
+	for inp in inputs {
+		res, indices := is_valid(inp.sudoku) or {
+			eprintln(err.msg)
+			false, map[int][]int{}
+		}
+		assert !res
+		assert indices == inp.indices
 	}
+}
+
+struct ErrInput {
+	sudoku  [][]int
+	err_msg string
+}
+
+fn test_sudoku_errors() {
+	mut inputs := [
+		ErrInput{[[0], [1], [2], [3], [4], [5], [6]], 'Expected 9 rows, got 7'},
+		ErrInput{[[0, 1, 2, 3, 4], [1], [2], [3], [4], [5], [6],
+			[7], [8]], 'Expected 9 columns, but row 1 only has 5'},
+		ErrInput{[[0, 1, 2, 3, 4, 5, 6, 7, 8], [1, 2, 3], [2],
+			[3], [4], [5], [6], [7], [8]], 'Expected 9 columns, but row 2 only has 3'},
+	]
+	mut expexted_errors := inputs.len
+	for inp in inputs {
+		is_valid(inp.sudoku) or {
+			expexted_errors--
+			assert err.msg == inp.err_msg
+		}
+	}
+	assert expexted_errors == 0
 }
