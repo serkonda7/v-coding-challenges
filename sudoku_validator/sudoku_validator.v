2aeb0cdeae6615008abaf6d7f65c13ff1cfb028f
Lukas Neubert <serkonda@protonmail.com>
2021-10-02 17:23:22 +0200

    sudoku validator: minor updates

--- sudoku_validator/sudoku_validator.v
+++ sudoku_validator/sudoku_validator.v
@@ -1,16 +1,17 @@
-/*
-Sudoku Validator
-
-A solved sudoku puzzle is a matrix of typically 9*9 in which every row, every column and every block contains every number from 1 to 9 exactly once.
-
-A block is a 3*3 area in the sudoku that you get by splitting it up into 9 even squares (similar to a tic-tac-toe board).
-
-Create an algorithm that checks any solved sudoku's validity.
-
-Bonus: Change the code to mark all mistakes in the sudoku puzzle.
-*/
 module sudoku_validator
 
+/*
+A solved sudoku puzzle is a matrix of typically 9*9 in which every row, every column
+and every block contains every number from 1 to 9 exactly once.
+
+> A block is a 3*3 area in the sudoku that you get by splitting it up into 9 even squares.
+
+Create an algorithm that checks any solved sudoku's validity.
+Bonus: Change the code to mark all mistakes in the sudoku puzzle.
+*/
+
+// is_valid returns whether a solved sudoku matrix is correct
+// and a map of all wrong fields per row
 pub fn is_valid(sudoku_rows [][]int) ?(bool, map[int][]int) {
 	if sudoku_rows.len != 9 {
 		return error('Expected 9 rows, got $sudoku_rows.len')
