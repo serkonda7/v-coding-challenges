ff825df26d8482dbea4da5eafa6d94cd09d1d80b
Lukas Neubert <serkonda@protonmail.com>
2021-10-03 09:35:11 +0200

    password validator
    - define rules as consts
    - doc comments

--- password_validator/password_validator.v
+++ password_validator/password_validator.v
@@ -1,44 +1,54 @@
+module password_validator
+
 /*
-Password Validator
-
 Password validator is a program that validates passwords to match specific rules.
-
-A valid password is the one that conforms to the following rules:
- - Minimum length is 6.
- - Maximum length is 14.
- - Should contain at least one number.
- - Should contain at least one uppercase and one lowercase letter.
- - Should contain at least one special character (such as &, +, @, $, #, %, etc.).
- - Should not contain spaces.
 
 Write a program to checks if the user input is a valid password or not.
 */
-module password_validator
-
 import regex
 
+// Rule definitions
+const (
+	min_len         = 6
+	max_len         = 14
+	require_num     = true
+	require_lower   = true
+	require_upper   = true
+	require_special = true
+	allow_spaces    = false
+)
+
+// is_valid checks whether a password conforms with the defined rules
 pub fn is_valid(password string) bool {
-	if password.len < 6 || password.len > 14 {
+	if password.len < password_validator.min_len || password.len > password_validator.max_len {
 		return false
 	}
-	if password.count(' ') > 0 {
+	if !password_validator.allow_spaces && password.count(' ') > 0 {
 		return false
 	}
-	mut number_re := regex.regex_opt(r'\d') or { panic(err) }
-	if number_re.find_all(password) == []int{} {
-		return false
+	if password_validator.require_num {
+		mut number_re := regex.regex_opt(r'\d') or { panic(err) }
+		if number_re.find_all(password).len == 0 {
+			return false
+		}
 	}
-	mut lower_re := regex.regex_opt(r'\a') or { panic(err) }
-	if lower_re.find_all(password) == []int{} {
-		return false
+	if password_validator.require_lower {
+		mut lower_re := regex.regex_opt(r'\a') or { panic(err) }
+		if lower_re.find_all(password).len == 0 {
+			return false
+		}
 	}
-	mut upper_re := regex.regex_opt(r'\A') or { panic(err) }
-	if upper_re.find_all(password) == []int{} {
-		return false
+	if password_validator.require_upper {
+		mut upper_re := regex.regex_opt(r'\A') or { panic(err) }
+		if upper_re.find_all(password).len == 0 {
+			return false
+		}
 	}
-	mut special_re := regex.regex_opt(r'[^A-Za-z0-9]') or { panic(err) }
-	if special_re.find_all(password) == []int{} {
-		return false
+	if password_validator.require_special {
+		mut special_re := regex.regex_opt(r'[^A-Za-z0-9]') or { panic(err) }
+		if special_re.find_all(password).len == 0 {
+			return false
+		}
 	}
 	return true
 }
