b80abfbbb61dd24aaa7e098f6e15cc356712e9ec
Lukas Neubert <serkonda@protonmail.com>
2021-10-03 10:15:10 +0200

    palindrome numbers: doc comment

--- palindrome_numbers/palindrome_numbers.v
+++ palindrome_numbers/palindrome_numbers.v
@@ -1,12 +1,12 @@
-/*
-Palindrome Numbers
+module palindrome_numbers
 
+/*
 Palindromic number is a number that is the same when written forwards or backwards.
 
 Create a program that takes a number as input and return whether the number is palindromic, or not.
 */
-module palindrome_numbers
 
+// is_palindrome checks whether a number is the same written forwards or backwards
 pub fn is_palindrome(num int) bool {
 	return num.str() == num.str().reverse()
 }
--- palindrome_numbers/palindrome_numbers_test.v
+++ palindrome_numbers/palindrome_numbers_test.v
@@ -3,12 +3,10 @@ module palindrome_numbers
 fn test_is_palindrome() {
 	palindromes := [656, 111, 7337]
 	for num in palindromes {
-		res := is_palindrome(num)
-		assert res == true
+		assert is_palindrome(num)
 	}
 	others := [234, 42, 7347]
 	for num in others {
-		res := is_palindrome(num)
-		assert res == false
+		assert !is_palindrome(num)
 	}
 }
