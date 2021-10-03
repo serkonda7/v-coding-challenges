c0a422688df2e4e47bd55884df11fae8fd51c260
Lukas Neubert <serkonda@protonmail.com>
2021-10-03 11:23:18 +0200

    vowel counter: doc comment

--- vowel_counter/vowel_counter.v
+++ vowel_counter/vowel_counter.v
@@ -1,16 +1,14 @@
-/*
-Vowels
+module vowel_counter
 
+/*
 Five of the 26 English alphabet letters are vowels: A, E, I, O, and U.
 
 Write a program that takes string input and outputs the count of the vowels in the string.
 */
-module vowel_counter
 
-const (
-	vowel_str = 'aeiou'
-)
+const vowel_str = 'aeiou'
 
+// vowel_count returns the number of vowels in a given string
 pub fn vowel_count(str string) int {
 	chars := str.to_lower().split('')
 	vowels := chars.filter(vowel_counter.vowel_str.contains(it))
